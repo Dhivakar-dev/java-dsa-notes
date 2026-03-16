91ce7487db4d3dc58096db2a25c9ea32b9c29191
Dhivakar <dhivakar449@gmail.com>
2026-03-16 17:06:13 +0530

    Best time to Buy and Sell Stock

--- leetcode/slidingwindow/BestTimeToBuyAndSellStock.java
+++ leetcode/slidingwindow/BestTimeToBuyAndSellStock.java
@@ -5,17 +5,16 @@ public class BestTimeToBuyAndSellStock {
 
         int min = prices[0];
         int profit = 0;
-        for(int i=0; i<prices.length; i++)
-        {
-            if(prices[i]<min)
-            {
+
+        for(int i=0; i<prices.length; i++) {
+            if(prices[i] < min) {
                 min = prices[i];
             }
 
             profit = Math.max(profit, prices[i]-min);
-
         }
 
         return profit;
+
     }
 }
